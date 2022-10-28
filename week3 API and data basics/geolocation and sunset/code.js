20b7c68b49e70d6844ba4ad3267865a11b9d29a7
Dave Webb <dave.webb@cranbim.com>
2022-10-28 17:50:47 +0100

    Week 3 examples

---
+++ week3 API and data basics/geolocation and sunset/code.js
@@ -0,0 +1,26 @@
+console.log("lets start using API's")
+
+if (!navigator.geolocation) {
+    // handle the error with a message
+} else {
+    navigator.geolocation.getCurrentPosition(location => {
+        let myLat=location.coords.latitude
+        let myLon=location.coords.longitude
+        getSunriseSunset(myLat, myLon)
+    });
+}
+
+function getSunriseSunset(lat, lon){
+    let query='https://api.sunrise-sunset.org/json?'+
+        'lat='+lat+
+        '&lng='+lon+
+        '&date=today'
+    fetch(query)
+        .then(response=>response.json())
+        .then(data=>{
+            console.log("sunrise: "+data.results.sunrise)
+            console.log("sunset: "+data.results.sunset)
+        })
+}
+
+
