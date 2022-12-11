787f340ff73c14ab85058390909bc3aaa1cb528f
Dave Webb <dave.webb@cranbim.com>
2022-12-11 09:08:24 +0000

    added p5 drawing to week 4

--- week9/node data server/public/main.js
+++ week9/node data server/public/main.js
@@ -22,23 +22,34 @@ function fetchData(howMany){
 
 function handleDataFromServer(receivedData){
     console.log('received from server ...')
-    // console.log(receivedData)
-    displayedData.innerHTML=''
-    myData.data=[]
-    receivedData.data.forEach((item,i)=>{
-        myData.status='updated'
-        myData.data.push({
-            animal: item.class,
-            value: item.value
-        })
-        // console.log(i)
-        // console.log(item)
-        if(item.class=='squirrel'){
-            let newEntry=document.createElement('li')
-            let cleanValue=parseInt(item.value*100)/100
-            newEntry.innerText=`animal: ${item.class}, value: ${cleanValue}`
-            displayedData.appendChild(newEntry)
+    console.log(receivedData.data)
+    let cleanData=[]
+    receivedData.data.forEach(item=>{
+        // console.log("the animal is a "+item.class+" and its value is "+ item.value)
+        if(item.class=='rabbit'){
+            cleanData.push({
+                animal: item.class,
+                value: parseInt(item.value*100)/100
+            })
         }
     })
+    console.log(cleanData)
+    // displayedData.innerHTML=''
+    // myData.data=[]
+    // receivedData.data.forEach((item,i)=>{
+    //     myData.status='updated'
+    //     myData.data.push({
+    //         animal: item.class,
+    //         value: item.value
+    //     })
+    //     // console.log(i)
+    //     // console.log(item)
+    //     if(item.class=='squirrel'){
+    //         let newEntry=document.createElement('li')
+    //         let cleanValue=parseInt(item.value*100)/100
+    //         newEntry.innerText=`animal: ${item.class}, value: ${cleanValue}`
+    //         displayedData.appendChild(newEntry)
+    //     }
+    // })
 
 }
