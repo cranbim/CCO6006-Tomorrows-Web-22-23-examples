719adc5191f6340cb41b13ecd3cfc542f513362e
Dave Webb <dave.webb@cranbim.com>
2023-01-04 12:09:39 +0000

    Week 11 initial API secret stuff

---
+++ week11/API keys/code.js
@@ -0,0 +1,11 @@
+const myVisibleAPIkey="ABCxyz123456"
+const myURLBase="https://someapi.coolapis.com/"
+
+//let apiQueryURL=`${myURLBase}?api_key=${myVisibleAPIkey}`
+let apiQueryURL=myURLBase+"?api_key="+myVisibleAPIkey
+
+console.log(apiQueryURL)
+
+apiQueryURL=myURLBase+"?api_key="+mySecretAPIKey
+
+console.log(apiQueryURL)
